17eb8551609f9db6fa6c498a7e58f53223a59668
Frederik Funke <frederik-funke@gmx.de>
2018-10-31 20:18:54 +0100

    Updates

---
+++ precache-manifest.19fb0fe5531109da9d2bf4a6d3d55e81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e1f325243b30ef2b08",
+    "url": "/fcc_frontend_pomodoro_clock/static/css/main.18d80bb5.chunk.css"
+  },
+  {
+    "revision": "82e1f325243b30ef2b08",
+    "url": "/fcc_frontend_pomodoro_clock/static/js/main.82e1f325.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/fcc_frontend_pomodoro_clock/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "d0603e0e2a01978ab375",
+    "url": "/fcc_frontend_pomodoro_clock/static/js/runtime~main.d0603e0e.js"
+  },
+  {
+    "revision": "7e4712fbd1c891e9ff10ae89a4ebfdb3",
+    "url": "/fcc_frontend_pomodoro_clock/index.html"
+  }
+];
